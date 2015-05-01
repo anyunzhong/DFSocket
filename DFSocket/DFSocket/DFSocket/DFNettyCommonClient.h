bffa2c802fab3bcb830c1085be58d4aa818a1818
Fast-Dev-Kit <2642754767@qq.com>
2015-05-02 02:50:55 +0800

    init

---
+++ DFSocket/DFSocket/DFSocket/DFNettyCommonClient.h
@@ -0,0 +1,51 @@
+//
+//  DFNettyChatClient.h
+//  netty-chat-client
+//
+//  Created by Allen Zhong on 15/3/15.
+//  Copyright (c) 2015年 Datafans Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GCDAsyncSocket.h"
+
+typedef NS_ENUM(NSUInteger, CONNECTION_STATUS){
+    RUNNING,CLOSED,CONNECTING,CLOSING,
+};
+
+#define HEARTBEAT_TIMEOUT_INTERVAL 60
+#define HEARTBEAT_SEND_INTERVAL 5
+
+#define kNotifyStatus @"notify_status"
+#define kStatus @"status"
+#define kNotifyDataReceived @"notify_data_received"
+#define kDataPackage @"data_package"
+
+
+
+@interface DFNettyCommonClient : NSObject <GCDAsyncSocketDelegate>
+
+@property (nonatomic,assign) BOOL hasMarkedRead;
+@property (nonatomic,assign) NSUInteger lastActiveTime;
+
+@property (nonatomic,assign) NSInteger reConnectTimes;
+
+@property (nonatomic,assign) CONNECTION_STATUS status;
+@property (nonatomic,strong) GCDAsyncSocket *socket;
+
+@property (nonatomic,strong) NSTimer *autoRestartTimer;
+@property (nonatomic,strong) NSTimer *autoSendHeartbeatTimer;
+@property (nonatomic,strong) NSTimer *heartbeatTimeoutCheckTimer;
+
+@property (nonatomic,strong) NSString *host;
+@property (nonatomic,assign) NSInteger port;
+
+-(void) connect:(NSString *) host port:(NSInteger) port;
+-(void) disconnect;
+
+-(void) write:(NSData *) data;
+
+-(void) write:(NSData *) data tag:(NSUInteger) tag;
+
+
+@end
---
+++ DFSocket/DFSocket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DFSocket
+//
+//  Created by Allen Zhong on 15/5/2.
+//  Copyright (c) 2015年 Datafans, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
