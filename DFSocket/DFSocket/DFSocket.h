aeda0f3018f6c11ca03467681181c36070a8d3e1
Fast-Dev-Kit <2642754767@qq.com>
2015-05-07 20:16:49 +0800

    add DFSocket.h

---
+++ DFSocket/DFSocket/DFSocket.h
@@ -0,0 +1,14 @@
+//
+//  DFSocket.h
+//  coder
+//
+//  Created by Allen Zhong on 15/5/7.
+//  Copyright (c) 2015年 Datafans, Inc. All rights reserved.
+//
+
+#ifndef coder_DFSocket_h
+#define coder_DFSocket_h
+
+#import "DFNettyCommonClient.h"
+
+#endif
